b446f0d8758e28a689bb3e7ed2664fd127e9d27e
codingsfirst786 <codingsfirst@gmail.com>
2025-02-24 02:06:55 +0500

    Hi

--- src/Components/Home/Cards.jsx
+++ src/Components/Home/Cards.jsx
@@ -112,16 +112,18 @@ const StatCard = ({ title, count, count24, bg }) => {
       className={`bg-Background px-2 shadow-xl shadow-[#00000079] py-3 w-1/2 rounded-lg ${bg}`}
     >
       <p className="text-textColor3 text-base flex items-center gap-2">
-        {title}
+        {/* {title} */}
         <span className="bg-[#5c5c5c] rounded-full p-1">
           <BsShare className="text-textColor3" />
         </span>
       </p>
-      <p className="text-3xl text-textColor3 font-semibold mt-1">{count}</p>
+      <p className="text-3xl text-textColor3 font-semibold mt-1">
+      {/* {count} */}
+      </p>
       <div className="w-[85%] mx-auto mt-7 flex justify-between p-1 rounded-full bg-[#a67912] bg-opacity-20">
         <div className="flex items-center font-medium text-xl text-green-600">
           <GoArrowUp />
-          {count24}
+          {/* {count24} */}0
         </div>
         <div className="gradient-circle"></div>
       </div>
