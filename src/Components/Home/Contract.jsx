6caba52e9f7f5e579235a1db5d4c248bb9c78936
Aqibmalik57 <aqibmalik1586@gmail.com>
2025-02-24 16:14:10 +0500

    adding the partner aand members on home

--- src/Components/Home/Contract.jsx
+++ src/Components/Home/Contract.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { BsTriangleFill } from "react-icons/bs";
-import { HiLink } from "react-icons/hi";
-import { IoCopy } from "react-icons/io5";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BsTriangleFill } from 'react-icons/bs';
+import { HiLink } from 'react-icons/hi';
+import { IoCopy } from 'react-icons/io5';
 
 const Contract = ({ ApiUrl }) => {
   const [isVisible, setIsVisible] = useState(true);
@@ -11,7 +11,7 @@ const Contract = ({ ApiUrl }) => {
   const [totalTransactions, setTotalTransactions] = useState(0);
   const [totalTurnover, setTotalTurnover] = useState(0);
   const [last24HoursTurnover, setLast24HoursTurnover] = useState(0);
-  const [filter, setFilter] = useState("all");
+  const [filter, setFilter] = useState('all');
 
   useEffect(() => {
     axios.get(`https://reffaralmoney.com/getAllUsers`).then((res) => {
@@ -36,8 +36,8 @@ const Contract = ({ ApiUrl }) => {
       const last24HoursTurnover = users.reduce((sum, user) => {
         return (
           sum +
-          user.transactions
-            .filter((txn) => new Date(txn.timestamp) >= last24Hours)
+          user?.transactions
+            ?.filter((txn) => new Date(txn.timestamp) >= last24Hours)
             .reduce(
               (txnSum, txn) =>
                 txnSum + parseFloat(txn.amount?.$numberDecimal || 0),
@@ -62,27 +62,27 @@ const Contract = ({ ApiUrl }) => {
         setTimeout(() => setShowToast(false), 2000);
       })
       .catch((error) => {
-        console.error("Failed to copy text: ", error);
+        console.error('Failed to copy text: ', error);
       });
   };
 
   return (
     <>
-      <div className="bg-Background shadow-xl shadow-[#00000079] w-full h-full rounded-lg mt-1 pb-4">
+      <div className='bg-Background shadow-xl shadow-[#00000079] w-full h-full rounded-lg mt-1 pb-4'>
         {showToast && (
-          <div className="fixed top-5 right-5 z-50 bg-gray-800 text-gray-200 py-2 px-4 rounded-lg shadow-2xl animate-quickAlert">
+          <div className='fixed top-5 right-5 z-50 bg-gray-800 text-gray-200 py-2 px-4 rounded-lg shadow-2xl animate-quickAlert'>
             🔗 Link copied!
           </div>
         )}
-        <h1 className="text-textColor2 border-b border-textColor2 flex justify-between items-center px-3 py-4">
+        <h1 className='text-textColor2 border-b border-textColor2 flex justify-between items-center px-3 py-4'>
           The Eagles USDT contract
           <span
-            className="bg-[#a67912] h-8 w-8 flex justify-center items-center rounded-full cursor-pointer"
+            className='bg-[#a67912] h-8 w-8 flex justify-center items-center rounded-full cursor-pointer'
             onClick={toggleVisibility}
           >
             <BsTriangleFill
               className={`text-textColor3 transform transition-transform duration-300 ${
-                isVisible ? "rotate-180" : "rotate-0"
+                isVisible ? 'rotate-180' : 'rotate-0'
               }`}
             />
           </span>
@@ -90,14 +90,14 @@ const Contract = ({ ApiUrl }) => {
 
         {isVisible && (
           <div>
-            <div className="flex justify-between py-4 px-3 border-b border-textColor2">
-              <p className="text-textColor2">x1/x2</p>
-              <p className="flex gap-2 text-textColor3 items-center">
-                0x55...a32{" "}
+            <div className='flex justify-between py-4 px-3 border-b border-textColor2'>
+              <p className='text-textColor2'>x1/x2</p>
+              <p className='flex gap-2 text-textColor3 items-center'>
+                0x55...a32{' '}
                 <span>
                   <IoCopy
-                    onClick={() => handleCopy("theeagles.io/******")}
-                    className="cursor-pointer"
+                    onClick={() => handleCopy('theeagles.io/******')}
+                    className='cursor-pointer'
                   />
                 </span>
                 <span>
@@ -105,36 +105,33 @@ const Contract = ({ ApiUrl }) => {
                 </span>
               </p>
             </div>
-            <div className="py-4 px-3 border-b border-textColor2">
-              <label className="text-textColor2">Filter: </label>
+            <div className='py-4 px-3 border-b border-textColor2'>
+              <label className='text-textColor2'>Filter: </label>
               <select
-                className="bg-[#a67912] text-white rounded px-2 py-1"
+                className='bg-[#a67912] text-white rounded px-2 py-1'
                 value={filter}
                 onChange={(e) => setFilter(e.target.value)}
               >
-                <option value="all">All Time</option>
-                <option value="24h">Last 24 Hours</option>
+                <option value='all'>All Time</option>
+                <option value='24h'>Last 24 Hours</option>
               </select>
             </div>
-            <div className="space-y-1 py-4 px-3 border-b border-textColor2">
-              <h1 className="text-textColor2">Total Users</h1>
-              <p className="text-[#a67912]">{totalUsers.toLocaleString()}</p>
+            <div className='space-y-1 py-4 px-3 border-b border-textColor2'>
+              <h1 className='text-textColor2'>Total Users</h1>
+              <p className='text-[#a67912]'>{totalUsers.toLocaleString()}</p>
             </div>
-            <div className="space-y-1 py-4 px-3 border-b border-textColor2">
-              <h1 className="text-textColor2">Transactions Made</h1>
-              <p className="text-[#a67912]">
+            <div className='space-y-1 py-4 px-3 border-b border-textColor2'>
+              <h1 className='text-textColor2'>Transactions Made</h1>
+              <p className='text-[#a67912]'>
                 {totalTransactions.toLocaleString()}
               </p>
-              <p className="text-white flex">
-              </p>
+              <p className='text-white flex'></p>
             </div>
-            <div className="space-y-1 pt-4 px-3">
-              <h1 className="text-textColor2">Turnover, USDT</h1>
-              <p className="text-[#a67912]">
-              {totalTurnover}
-              </p>
-              <p className="text-white flex">
-              {filter === "24h" ? last24HoursTurnover : totalTurnover}
+            <div className='space-y-1 pt-4 px-3'>
+              <h1 className='text-textColor2'>Turnover, USDT</h1>
+              <p className='text-[#a67912]'>{totalTurnover}</p>
+              <p className='text-white flex'>
+                {filter === '24h' ? last24HoursTurnover : totalTurnover}
               </p>
             </div>
           </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,21 @@
-import { GoArrowUpRight } from "react-icons/go";
-import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
-import { IoCopy, IoPersonCircleSharp, IoSettingsSharp } from "react-icons/io5";
-import { useEffect, useState } from "react";
-import { FaHeart } from "react-icons/fa";
-import { HiMiniXMark } from "react-icons/hi2";
-import { useConnect, useDisconnect, useAccount, useSwitchChain } from "wagmi";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import Cards from "../Components/Home/Cards";
-import Program from "../Components/Home/Program";
-import Members from "../Components/Home/Members";
-import Contract from "../Components/Home/Contract";
-import History from "../Components/Home/History";
-import chainConfig from "../Config/chainConfig";
-import { ApiUrl } from "../Config/config";
-import { users } from "../Config/Contract-Methods";
-import { ToastContainer, toast } from "react-toastify";
+import { GoArrowUpRight } from 'react-icons/go';
+import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
+import { IoCopy, IoPersonCircleSharp, IoSettingsSharp } from 'react-icons/io5';
+import { useEffect, useState } from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { HiMiniXMark } from 'react-icons/hi2';
+import { useConnect, useDisconnect, useAccount, useSwitchChain } from 'wagmi';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Cards from '../Components/Home/Cards';
+import Program from '../Components/Home/Program';
+import Members from '../Components/Home/Members';
+import Contract from '../Components/Home/Contract';
+import History from '../Components/Home/History';
+import chainConfig from '../Config/chainConfig';
+import { ApiUrl } from '../Config/config';
+import { users } from '../Config/Contract-Methods';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Home = ({ showBar, setShowBar, user }) => {
   const [showDetails, setShowDetails] = useState(true);
@@ -27,9 +27,9 @@ const Home = ({ showBar, setShowBar, user }) => {
   const [showToast, setShowToast] = useState(false);
   const [userData, setUserData] = useState([]);
   const [referralData, setReferralData] = useState(null);
-  const [PT, setPT] = useState("");
+  const [PT, setPT] = useState('');
 
-  const userID = localStorage.getItem("id");
+  const userID = localStorage.getItem('id');
 
   const handleCopy = (textToCopy) => {
     navigator.clipboard
@@ -48,7 +48,7 @@ const Home = ({ showBar, setShowBar, user }) => {
         const result = await users(address);
         if (result?.[1]) {
           const userId = result[1];
-          localStorage.setItem("id", result[1].toString());
+          localStorage.setItem('id', result[1].toString());
           setUserData(result);
           const referralResponse = await axios.get(
             `${ApiUrl}/refferal/${userId}`
@@ -57,7 +57,7 @@ const Home = ({ showBar, setShowBar, user }) => {
           setPT(referralResponse?.data);
         }
       } catch (error) {
-        console.error("Fetch error:", error);
+        console.error('Fetch error:', error);
       } finally {
         setLoading(false);
       }
@@ -85,48 +85,48 @@ const Home = ({ showBar, setShowBar, user }) => {
   const wallets = [
     {
       id: 1,
-      name: "Trust Wallet",
-      description: "DApp in App",
-      image: "/assets/AuthImages/trust.png",
+      name: 'Trust Wallet',
+      description: 'DApp in App',
+      image: '/assets/AuthImages/trust.png',
     },
     {
       id: 2,
-      name: "TokenPocket",
-      description: "DApp in App",
-      image: "/assets/AuthImages/pocket.png",
+      name: 'TokenPocket',
+      description: 'DApp in App',
+      image: '/assets/AuthImages/pocket.png',
     },
     {
       id: 3,
-      name: "MetaMask",
-      description: "DApp in App",
-      image: "/assets/AuthImages/Mask.png",
+      name: 'MetaMask',
+      description: 'DApp in App',
+      image: '/assets/AuthImages/Mask.png',
     },
     {
       id: 4,
-      name: "WalletConnect",
-      description: "Any Wallet and browser",
-      image: "/assets/AuthImages/connect.png",
+      name: 'WalletConnect',
+      description: 'Any Wallet and browser',
+      image: '/assets/AuthImages/connect.png',
     },
   ];
 
   return (
     <>
       {showToast && (
-        <div className="fixed top-5 right-5 bg-gray-800 text-gray-200 py-2 px-4 rounded-lg shadow-2xl transform animate-quickAlert">
+        <div className='fixed top-5 right-5 bg-gray-800 text-gray-200 py-2 px-4 rounded-lg shadow-2xl transform animate-quickAlert'>
           🔗 Link copied!
         </div>
       )}
       <ToastContainer />
-      <div className="relative overflow-hidden">
+      <div className='relative overflow-hidden'>
         <div
           className={`absolute top-0 h-screen w-full bg-black py-4 px-3 transition-all duration-500 z-50 ${
-            showBar ? "right-0" : "-right-full"
+            showBar ? 'right-0' : '-right-full'
           }`}
         >
-          <div className="flex justify-end">
-            <div className="inline-block bg-Background p-2 rounded-full shadow-2xl">
+          <div className='flex justify-end'>
+            <div className='inline-block bg-Background p-2 rounded-full shadow-2xl'>
               <HiMiniXMark
-                className="text-white text-3xl"
+                className='text-white text-3xl'
                 onClick={() => setShowBar(false)}
               />
             </div>
@@ -135,31 +135,31 @@ const Home = ({ showBar, setShowBar, user }) => {
             <div
               key={wallet.id}
               onClick={() => handleConnect(wallet.name)}
-              className="cursor-pointer mt-3 bg-zinc-900 text-textColor2 rounded-lg flex items-center gap-6 py-5 px-3"
+              className='cursor-pointer mt-3 bg-zinc-900 text-textColor2 rounded-lg flex items-center gap-6 py-5 px-3'
             >
-              <div className="h-16 w-16 bg-textColor3 rounded-full flex justify-center items-center">
+              <div className='h-16 w-16 bg-textColor3 rounded-full flex justify-center items-center'>
                 <img
                   src={wallet.image}
                   alt={wallet.name}
-                  className="h-[48px] w-[48px]"
+                  className='h-[48px] w-[48px]'
                 />
               </div>
               <div>
-                <h1 className="text-2xl font-medium text-textColor3">
+                <h1 className='text-2xl font-medium text-textColor3'>
                   {wallet.name}
                 </h1>
-                <p className="text-xs">{wallet.description}</p>
+                <p className='text-xs'>{wallet.description}</p>
               </div>
             </div>
           ))}
-          <p className="text-textColor2 text-center mt-16 text-sm">
-            Got a Question?{" "}
-            <span className="text-textColor3 font-medium">Contact Support</span>
+          <p className='text-textColor2 text-center mt-16 text-sm'>
+            Got a Question?{' '}
+            <span className='text-textColor3 font-medium'>Contact Support</span>
           </p>
         </div>
 
-        <div className="w-full px-4 pt-6 pb-5 homebg bg-[#a67a1240]">
-          <div className="relative">
+        <div className='w-full px-4 pt-6 pb-5 homebg bg-[#a67a1240]'>
+          <div className='relative'>
             {/* <div className="absolute inset-0 h-full opacity-10 left-20 -top-3">
               <img
                 src="assets/HomeImages/eaglebg.jpg"
@@ -168,27 +168,27 @@ const Home = ({ showBar, setShowBar, user }) => {
               />
             </div> */}
 
-            <div className="bg-eagles relative inset-0 z-10">
+            <div className='bg-eagles relative inset-0 z-10'>
               <div
                 className={`h-auto flex justify-between ${
-                  userData?.[1] > 1 ? "" : "pb-10"
+                  userData?.[1] > 1 ? '' : 'pb-10'
                 }`}
               >
-                <div className="flex gap-6">
-                  <div className="gradient-border h-20 w-20 rounded-full ms-3">
-                    <div className="relative flex items-center justify-center text-white h-full w-full rounded-full bg-Background">
-                      <IoPersonCircleSharp className="text-7xl text-textColor3" />
+                <div className='flex gap-6'>
+                  <div className='gradient-border h-20 w-20 rounded-full ms-3'>
+                    <div className='relative flex items-center justify-center text-white h-full w-full rounded-full bg-Background'>
+                      <IoPersonCircleSharp className='text-7xl text-textColor3' />
                       <img
-                        src="/assets/HomeImages/logo.png"
-                        alt="logo"
-                        className="h-7 w-7 ms-2 object-cover absolute bottom-2 -right-6 rounded-full"
+                        src='/assets/HomeImages/logo.png'
+                        alt='logo'
+                        className='h-7 w-7 ms-2 object-cover absolute bottom-2 -right-6 rounded-full'
                       />
-                      <Link to="/social" className="absolute -bottom-7">
-                        <div className="w-28 rounded-full p-[2px] bg-gradient-to-r from-[#a67912] via-white to-white">
-                          <div className="bg-[#433108] rounded-full text-xs px-2 flex items-center justify-between py-1">
-                            <p className="text-slate-400">Social</p>
-                            <span className="flex gap-1 items-center">
-                              <FaHeart className="text-pink-400" />
+                      <Link to='/social' className='absolute -bottom-7'>
+                        <div className='w-28 rounded-full p-[2px] bg-gradient-to-r from-[#a67912] via-white to-white'>
+                          <div className='bg-[#433108] rounded-full text-xs px-2 flex items-center justify-between py-1'>
+                            <p className='text-slate-400'>Social</p>
+                            <span className='flex gap-1 items-center'>
+                              <FaHeart className='text-pink-400' />
                               526
                             </span>
                           </div>
@@ -196,39 +196,39 @@ const Home = ({ showBar, setShowBar, user }) => {
                       </Link>
                     </div>
                   </div>
-                  <div className="text-textColor3 ml-10">
-                    <h1 className="text-2xl font-semibold font-sans capitalize">
-                      {user?.name || "Username"}
+                  <div className='text-textColor3 ml-10'>
+                    <h1 className='text-2xl font-semibold font-sans capitalize'>
+                      {user?.name || 'Username'}
                       {/* username kaa lia profile must bani honi chahiya  haa or wallet connect hons chahiya haa  */}
                     </h1>
                     {!loading && userData ? (
-                      <p className="text-lg text-yellow-300 italic font-medium w-fit shadow-xl shadow-[#00000079]">
-                        ID {userData?.[1]?.toString() || "Display Soon"}
+                      <p className='text-lg text-yellow-300 italic font-medium w-fit shadow-xl shadow-[#00000079]'>
+                        ID {userData?.[1]?.toString() || 'Display Soon'}
                       </p>
                     ) : null}
 
                     {userData?.[1] > 1 ? (
                       <button
-                        className="mt-8 text-base flex gap-2 items-center justify-center bg-Background shadow-xl shadow-[#00000079] transition-all ease-in-out text-textColor2 w-44 py-1 rounded-full"
+                        className='mt-8 text-base flex gap-2 items-center justify-center bg-Background shadow-xl shadow-[#00000079] transition-all ease-in-out text-textColor2 w-44 py-1 rounded-full'
                         onClick={() => setShowDetails(!showDetails)}
                       >
-                        {showDetails ? "Show less" : "Show more"}
+                        {showDetails ? 'Show less' : 'Show more'}
                         {showDetails ? <IoIosArrowUp /> : <IoIosArrowDown />}
                       </button>
                     ) : (
-                      ""
+                      ''
                     )}
                   </div>
                 </div>
                 <IoSettingsSharp
-                  className="text-textColor3 text-3xl mt-1 cursor-pointer"
-                  onClick={() => navigate("/profile")}
+                  className='text-textColor3 text-3xl mt-1 cursor-pointer'
+                  onClick={() => navigate('/profile')}
                 />
               </div>
 
               {showDetails && userData?.[1] > 1 && (
-                <div className="mt-1">
-                  <div className="flex gap-x-1 items-center text-sm text-textColor3">
+                <div className='mt-1'>
+                  <div className='flex gap-x-1 items-center text-sm text-textColor3'>
                     {referralData?.referrer ? (
                       <p>
                         {`${referralData.referrer.slice(
@@ -242,26 +242,26 @@ const Home = ({ showBar, setShowBar, user }) => {
 
                     <IoCopy
                       onClick={() => handleCopy(referralData?.referrer)}
-                      className="cursor-pointer"
+                      className='cursor-pointer'
                     />
                   </div>
 
-                  <div className="flex gap-2 items-center text-textColor3 text-sm mt-2">
+                  <div className='flex gap-2 items-center text-textColor3 text-sm mt-2'>
                     <p>
-                      Joined{" "}
+                      Joined{' '}
                       {referralData?.createdAt
                         ? new Date(referralData.createdAt).toLocaleDateString()
-                        : ""}
+                        : ''}
                     </p>
-                    <p className="px-1 flex justify-center text-yellow-300 shadow-lg shadow-[#00000079] font-medium text-base bg-[#333333] bg-opacity-35 rounded-full italic">
+                    <p className='px-1 flex justify-center text-yellow-300 shadow-lg shadow-[#00000079] font-medium text-base bg-[#333333] bg-opacity-35 rounded-full italic'>
                       ID {referralData?.id}
                     </p>
                   </div>
 
                   {referralData?.referrerId && (
-                    <div className="mt-2 text-textColor3 text-sm flex items-center gap-2">
+                    <div className='mt-2 text-textColor3 text-sm flex items-center gap-2'>
                       <span>Referred by ID:</span>
-                      <span className="px-2 py-1 bg-[#333333] bg-opacity-35 rounded-full">
+                      <span className='px-2 py-1 bg-[#333333] bg-opacity-35 rounded-full'>
                         {referralData.referrerId}
                       </span>
                     </div>
@@ -270,24 +270,24 @@ const Home = ({ showBar, setShowBar, user }) => {
               )}
             </div>
           </div>
-          <div className="bg-[#a67912] shadow-xl shadow-[#00000079] bg-opacity-20 w-full px-3 py-3 rounded-lg mt-3 mb-3">
-            <div className="flex items-center justify-between text-base mb-5">
-              <h5 className="text-textColor3">My Personal link</h5>
-              <p className="text-textColor3 text-base font-sans font-medium flex gap-2 items-center">
+          <div className='bg-[#a67912] shadow-xl shadow-[#00000079] bg-opacity-20 w-full px-3 py-3 rounded-lg mt-3 mb-3'>
+            <div className='flex items-center justify-between text-base mb-5'>
+              <h5 className='text-textColor3'>My Personal link</h5>
+              <p className='text-textColor3 text-base font-sans font-medium flex gap-2 items-center'>
                 theeagles.io/{userData?.[1]?.toString()}
-                <GoArrowUpRight className="text-lg" />
+                <GoArrowUpRight className='text-lg' />
               </p>
             </div>
-            <div className="text-lg flex gap-3">
+            <div className='text-lg flex gap-3'>
               <button
-                className="bg-[#a67912] w-full text-textColor3 shadow-xl shadow-[#00000079] font-medium px-6 py-1 rounded-full"
+                className='bg-[#a67912] w-full text-textColor3 shadow-xl shadow-[#00000079] font-medium px-6 py-1 rounded-full'
                 onClick={() =>
                   handleCopy(`theeagles.io/${userData?.[1]?.toString()}`)
                 }
               >
                 Copy
               </button>
-              <button className="w-full bg-textColor3 shadow-lg shadow-[#00000079] font-medium px-6 rounded-full">
+              <button className='w-full bg-textColor3 shadow-lg shadow-[#00000079] font-medium px-6 rounded-full'>
                 Share
               </button>
             </div>
