441a0f2647e0028114198cae064bab0961f35272
kashifali0969082 <kashifali0969082@gmail.com>
2025-03-18 22:50:35 +0500

    rpc updated

--- src/Config/config.js
+++ src/Config/config.js
@@ -36,7 +36,9 @@ export const config = createConfig({
 
   transports: {
     // [sepolia.id]: http(),
-    [bsc.id]: http(),
+    [bsc.id]: http("https://bsc-mainnet.infura.io/v3/f5778e9c8b764c2eb60678ad73f25586"),
+
+    
   },
 });
 
